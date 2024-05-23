38e2b18b7efff51eceb529253f3bb7dc248b27f4
Peter <pbreslin@tcd.ie>
2024-05-22 21:49:46 -0700

    more readable

--- may24/03.py
+++ may24/03.py
@@ -1,7 +1,10 @@
 """
-Given two version strings, version1 and version2, compare them. A version string consists of revisions separated by dots '.'. The value of the revision is its integer conversion ignoring leading zeros.
+Given two version strings, version1 and version2, compare them. 
+A version string consists of revisions separated by dots '.'. 
+The value of the revision is its integer conversion ignoring leading zeros.
 
-To compare version strings, compare their revision values in left-to-right order. If one of the version strings has fewer revisions, treat the missing revision values as 0.
+To compare version strings, compare their revision values in left-to-right order. 
+If one of the version strings has fewer revisions, treat the missing revision values as 0.
 
 Return the following:
 
